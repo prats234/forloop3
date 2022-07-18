873c1f06c31d5bde6530da1b3fa0bd76044ae4ad
prats234 <109065276+prats234@users.noreply.github.com>
2022-07-18 20:41:18 +0530

    Add files via upload

---
+++ forloop3.java
@@ -0,0 +1,15 @@
+public class forloop3
+{
+    public static void main (String[] args)
+    {
+        int n = 6;
+        for (int i = 0 ; i < 6; i++)
+        {
+            for(int j = 0; j <= i ; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
